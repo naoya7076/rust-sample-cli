f09d0f4402b2869caefea627d42448a87c4bc528
naoya7076 <naoya7076@gmail.com>
2022-03-22 22:45:24 +0900

    add comments

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args)
+    // {:?}と{}は何が違うの？
+    // この辺りが参考になりそう
+    // http://doc.rust-jp.rs/rust-by-example-ja/hello/print/print_display.html
 }
