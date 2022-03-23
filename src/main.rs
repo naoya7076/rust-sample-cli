e57ce30ed617dbbc14b86bc6904a17882e621fca
naoya7076 <naoya7076@gmail.com>
2022-03-23 22:08:26 +0900

    clapをderiveマクロで記述

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
-use clap::{Arg, Command};
+use clap::Parser;
+
+// まずderive記法とは
+//https://doc.rust-jp.rs/book-ja/ch19-06-macros.html?highlight=derive#%E3%83%9E%E3%82%AF%E3%83%AD
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "naoya",
+    about = "hoge"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = Command::new("My RPN program")
-        .version("1.0.0")
-        .author("naoya7076")
-        .about("RPN calculator sample")
-        .arg(
-            Arg::new("formula_file")
-                .help("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .help("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
-    match matches.value_of("formula_file") {
+    let opts = Opts::parse();
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose)
+    println!("Is verbosity specified?:{}", opts.verbose)
 }
