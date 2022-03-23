25e9b3a36ee12a3fece85fa5b1f3e86741001a4c
naoya7076 <naoya7076@gmail.com>
2022-03-23 09:26:43 +0900

    builder パターンで作成

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use std::env;
+use clap::{Arg, Command};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
-    // {:?}と{}は何が違うの？
-    // この辺りが参考になりそう
-    // http://doc.rust-jp.rs/rust-by-example-ja/hello/print/print_display.html
+    let matches = Command::new("My RPN program")
+        .version("1.0.0")
+        .author("naoya7076")
+        .about("RPN calculator sample")
+        .arg(
+            Arg::new("formula_file")
+                .help("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .help("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose)
 }
