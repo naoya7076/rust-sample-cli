a504e1b09f6e3a69d044761e473be7696e1df681
naoya7076 <naoya7076@gmail.com>
2022-03-24 22:31:11 +0900

    BufReadで受け取れるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
 // まずderive記法とは
 //https://doc.rust-jp.rs/book-ja/ch19-06-macros.html?highlight=derive#%E3%83%9E%E3%82%AF%E3%83%AD
@@ -14,14 +16,31 @@ struct Opts {
     verbose: bool,
 
     #[clap(name = "FILE")]
+    // Optionはデータがあることと無いことを表す構造型
     formula_file: Option<String>,
 }
 
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+
+    // pathってどこでも定義していないのになぜ使えるの？
+    // formula_fileがある場合の値ってことか
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose)
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose)
     }
-    println!("Is verbosity specified?:{}", opts.verbose)
+}
+
+// lines()関数はBufReadというトレイトを実装していれば利用できる
+// traitがあまり分からないのでtrait.rsに実装
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line)
+    }
 }
