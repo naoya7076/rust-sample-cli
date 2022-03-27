984166969803abdeb37a2cb9c820de865cb86740
naoya7076 <naoya7076@gmail.com>
2022-03-27 17:51:49 +0900

    独自エラーの定義

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,19 +1,27 @@
-fn get_int_from_file() -> Result<i32, String> {
+enum MyError {
+    Io(std::io::Error),
+    Num(std::num::ParseIntError),
+}
+
+fn get_int_from_file() -> Result<i32, MyError> {
     let path = "number.txt";
 
     // ?はOK(t)ならtを返してErr(e)なら早期リターン
-    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    let num_str = std::fs::read_to_string(path).map_err(MyError::Io)?;
 
     num_str
         .trim()
         .parse::<i32>()
         .map(|t| t * 2)
-        .map_err(|e| e.to_string())
+        .map_err(MyError::Num)
 }
 
 fn main() {
     match get_int_from_file() {
         Ok(x) => println!("{}", x),
-        Err(e) => println!("{}", e),
+        Err(e) => match e {
+            MyError::Io(cause) => println!("I/O Error: {}", cause),
+            MyError::Num(cause) => println!("Parse Error: {}", cause),
+        },
     }
 }
