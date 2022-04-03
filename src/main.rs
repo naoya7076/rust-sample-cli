fedaff9ae6485a0d0bfdccc1e4d577027ba13da6
naoya7076 <naoya7076@gmail.com>
2022-04-03 10:18:24 +0900

    anyhowをRpnCalculatorに適用

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,61 @@
+use anyhow::{bail, ensure, Context, Result};
+
 use clap::Parser;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    // Selfは呼び出し元オブジェクトの型。この場合はRpnCalculator
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    // &strがわからん.まず&がわからん
+    pub fn eval(&self, formula: &str) -> Result<i32> {
+        // mutとVecがわからん
+        // 確かmutは可変の変数
+        // collectはイテレータをコレクションに変換する。変換先のコレクションを::<T>のように指定できる
+        // Vec<_>のように_で埋めておけば型推論される
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        // 所有権と借用の話
+        // 借用: 所有権は元の所有者が持ったまま、値の参照を貸し出すこと
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
+        let mut stack = Vec::new();
+        let mut pos = 0;
+
+        while let Some(token) = tokens.pop() {
+            pos += 1;
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x)
+            } else {
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => bail!("invalid token at {}", pos),
+                };
+                stack.push(res)
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        ensure!(stack.len() == 1, "invalid syntax");
+
+        Ok(stack[0])
+    }
+}
 
 // まずderive記法とは
 //https://doc.rust-jp.rs/book-ja/ch19-06-macros.html?highlight=derive#%E3%83%9E%E3%82%AF%E3%83%AD
@@ -20,66 +75,14 @@ struct Opts {
     formula_file: Option<String>,
 }
 
-struct RpnCalculator(bool);
-
-impl RpnCalculator {
-    // Selfは呼び出し元オブジェクトの型。この場合はRpnCalculator
-    pub fn new(verbose: bool) -> Self {
-        Self(verbose)
-    }
-
-    // &strがわからん.まず&がわからん
-    pub fn eval(&self, formula: &str) -> i32 {
-        // mutとVecがわからん
-        // 確かmutは可変の変数
-        // collectはイテレータをコレクションに変換する。変換先のコレクションを::<T>のように指定できる
-        // Vec<_>のように_で埋めておけば型推論される
-        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
-        // 所有権と借用の話
-        // 借用: 所有権は元の所有者が持ったまま、値の参照を貸し出すこと
-        self.eval_inner(&mut tokens)
-    }
-
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
-        let mut stack = Vec::new();
-
-        while let Some(token) = tokens.pop() {
-            if let Ok(x) = token.parse::<i32>() {
-                stack.push(x)
-            } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
-                let res = match token {
-                    "+" => x + y,
-                    "-" => x - y,
-                    "*" => x * y,
-                    "/" => x / y,
-                    "%" => x % y,
-                    _ => panic!("invalid token"),
-                };
-                stack.push(res)
-            }
-
-            if self.0 {
-                println!("{:?} {:?}", tokens, stack);
-            }
-        }
-
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("incalid syntax")
-        }
-    }
-}
-
-fn main() {
+// main関数ってResult返せれるのか。Result<()>の「()」は何だ
+fn main() -> Result<()> {
     let opts = Opts::parse();
 
     // pathってどこでも定義していないのになぜ使えるの？
     // formula_fileがある場合の値ってことか
     if let Some(path) = opts.formula_file {
-        let f = File::open(path).unwrap();
+        let f = File::open(path)?;
         let reader = BufReader::new(f);
         run(reader, opts.verbose)
     } else {
@@ -91,14 +94,18 @@ fn main() {
 
 // lines()関数はBufReadというトレイトを実装していれば利用できる
 // traitがあまり分からないのでtrait.rsで学習
-fn run<R: BufRead>(reader: R, verbose: bool) {
+// Result<()>の()はなんだろうか
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
     let calc = RpnCalculator::new(verbose);
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", answer)
+        let line = line?;
+        match calc.eval(&line) {
+            Ok(answer) => println!("{}", answer),
+            Err(e) => println!("{:#?}", e),
+        }
     }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -108,21 +115,23 @@ mod tests {
     #[test]
     fn text_ok() {
         let calc = RpnCalculator::new(false);
-        assert_eq!(calc.eval("5"), 5);
-        assert_eq!(calc.eval("50"), 50);
-        assert_eq!(calc.eval("-50"), -50);
+        assert_eq!(calc.eval("5").unwrap(), 5);
+        assert_eq!(calc.eval("50").unwrap(), 50);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
 
-        assert_eq!(calc.eval("2 3 +"), 5);
-        assert_eq!(calc.eval("2 3 *"), 6);
-        assert_eq!(calc.eval("2 3 - "), -1);
-        assert_eq!(calc.eval("2 3 /"), 0);
-        assert_eq!(calc.eval("2 3 %"), 2);
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("2 3 *").unwrap(), 6);
+        assert_eq!(calc.eval("2 3 - ").unwrap(), -1);
+        assert_eq!(calc.eval("2 3 /").unwrap(), 0);
+        assert_eq!(calc.eval("2 3 %").unwrap(), 2);
     }
 
     #[test]
     #[should_panic]
     fn test_ng() {
         let calc = RpnCalculator::new(false);
-        calc.eval("1 1 ^");
+        assert!(calc.eval("").is_err());
+        assert!(calc.eval("1 1 1 +").is_err());
+        assert!(calc.eval("+ 1 1").is_err());
     }
 }
